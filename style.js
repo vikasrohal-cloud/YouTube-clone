e9d3e26d453808adf1bbe98169927cc42c3ea116
vikas choudhary <cvikas861@gmail.com>
2023-02-25 15:54:53 +0530

    initial commit

---
+++ style.js
@@ -0,0 +1,8 @@
+
+var onmenuBar = document.querySelector(".onmenuBar")
+var menuBar = document.querySelector(".menuBar")
+
+onmenuBar.onclick = function(){
+    menuBar.classList.toggle("active")
+}
+
